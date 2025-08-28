9219af3c3b3eef3e9912f4e507b6fda41fed05e0
obaidur-0090 <mdobaidur0090@gmail.com>
2025-08-28 21:08:22 +0600

    heart icon

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
-document.getElementById("menu-btn").addEventListener("click",function(){
-    console.log("connected file")
-})
+
+const copyButtons = document.querySelectorAll('.copy-btn');
+const copyCountElement = document.getElementById("copy-count");
+
+
+let totalCount = parseInt(copyCountElement.innerText);
+
+
+for (let i = 0; i < copyButtons.length; i++) {
+    copyButtons[i].addEventListener("click", function(e){
+        e.preventDefault();
+
+        const card = copyButtons[i].closest('div.bg-white');
+
+        const cardHeader = card.querySelector('h1').innerText;
+
+        alert(` ${cardHeader}`);
+        totalCount += 1;
+        copyCountElement.innerText = totalCount;
+    });
+}
+
+const heartIcons = document.querySelectorAll(".heart-icon");
+
+const heartIconCount = document.getElementById("heart-icon-count");
+
+let heartCount = parseInt(heartIconCount.innerText);
+
+for (let i = 0; i < heartIcons.length; i++) {
+    heartIcons[i].addEventListener("click", function (e) {
+        e.preventDefault();
+        heartCount=heartCount+1;
+        heartIconCount.innerText = heartCount;
+    });
+}
+
