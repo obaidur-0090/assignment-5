3ebde23462d40fb8d57379e656f09c44776a1905
obaidur-0090 <mdobaidur0090@gmail.com>
2025-08-28 23:01:18 +0600

    all done

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let totalCount = parseInt(copyCountElement.innerText);
 
 
 for (let i = 0; i < copyButtons.length; i++) {
-    copyButtons[i].addEventListener("click", function(e){
+    copyButtons[i].addEventListener("click", function (e) {
         e.preventDefault();
 
         const card = copyButtons[i].closest('div.bg-white');
@@ -29,8 +29,66 @@ let heartCount = parseInt(heartIconCount.innerText);
 for (let i = 0; i < heartIcons.length; i++) {
     heartIcons[i].addEventListener("click", function (e) {
         e.preventDefault();
-        heartCount=heartCount+1;
+        heartCount = heartCount + 1;
         heartIconCount.innerText = heartCount;
     });
 }
 
+// call and history function
+
+const callBtn = document.querySelectorAll(".call-btn");
+const clearBtn = document.getElementById("clear-btn");
+const coinCount = document.getElementById("coin-count")
+let cointCountElement = parseInt(coinCount.innerText);
+
+const historyContainer = document.querySelector(".history-div")
+
+
+for (const callBtns of callBtn) {
+    callBtns.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        const call = callBtns.closest('.bg-white');
+        const callh1 = call.querySelector("h1").innerText;
+        const callh1Number = call.querySelector(".h1tag").innerText;
+        
+        
+        if (cointCountElement >= 20) {
+            alert(`${callh1} \n ${callh1Number} \n calling.....`);
+            cointCountElement = cointCountElement-20;
+            coinCount.innerText= cointCountElement;
+
+            const historyDiv = document.createElement("div");
+            historyDiv.classList.add("mt-2", "bg-gray-300", "p-5", "rounded-2xl", "flex", "justify-between", "items-center");
+
+            const time = new Date();
+            const currentTime = time.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+
+            historyDiv.innerHTML = `
+            <div>
+                <h1 class="font-bold">${callh1}</h1>
+                <p>${callh1Number}</p>
+            </div>
+            <div>
+                <p>${currentTime}</p>
+            </div>
+        `;
+
+            historyContainer.appendChild(historyDiv);
+        } else alert("তোমার পর্যাপ্ত পরিমাণ Coin নাই")
+
+
+
+    });
+}
+
+
+clearBtn.addEventListener("click", function () {
+
+    const histories = historyContainer.querySelectorAll(".mt-2");
+    histories.forEach((item, index) => {
+        if (index > 0) {
+            item.remove();
+        }
+    });
+});
